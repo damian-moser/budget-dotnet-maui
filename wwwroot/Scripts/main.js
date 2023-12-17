7e8d1d656cce90343dfcabeac415d141203ebd31
Damian Moser <damian.moser@sensemail.ch>
2023-12-17 20:41:29 +0100

    cards cash editing

---
+++ wwwroot/Scripts/main.js
@@ -0,0 +1,18 @@
+﻿function toggle() {
+    document.getElementById("popup").classList.toggle("visible");
+}
+
+function editCashValue(identifier, plus) {
+    const value = document.getElementById(identifier.dataset.cashType);
+    if (plus) {
+        const result = parseFloat(value.innerHTML) + parseFloat(identifier.dataset.cashType);
+        value.innerHTML = result.toFixed(2);
+    } else {
+        const result = parseFloat(value.innerHTML) - parseFloat(identifier.dataset.cashType);
+        value.innerHTML = result >= 0 ? result.toFixed(2) : 0;
+    }
+}
+
+function saveCash() {
+    
+}
