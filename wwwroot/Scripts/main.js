6003ad5ca5d1e571d27b9291420ecef83f524685
Damian Moser <damian.moser@sensemail.ch>
2023-12-24 23:11:26 +0100

    new card

--- wwwroot/Scripts/main.js
+++ wwwroot/Scripts/main.js
@@ -1,14 +1,7 @@
-﻿function toggle() {
-    document.getElementById("popup").classList.toggle("visible");
+﻿function editCash() {
+    document.getElementById("popup-cash").classList.toggle("visible");
 }
 
-function editCashValue(identifier, plus) {
-    const value = document.getElementById(identifier.dataset.cashType);
-    if (plus) {
-        const result = parseFloat(value.innerHTML) + parseFloat(identifier.dataset.cashType);
-        value.innerHTML = result.toFixed(2);
-    } else {
-        const result = parseFloat(value.innerHTML) - parseFloat(identifier.dataset.cashType);
-        value.innerHTML = result >= 0 ? result.toFixed(2) : 0;
-    }
+function newCard() {
+    document.getElementById("popup-new-card").classList.toggle("visible");
 }
