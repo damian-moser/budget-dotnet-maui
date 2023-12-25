97813a8a3ed7d896c9f98f18f38064e907a0cf5e
Damian Moser <damian.moser@sensemail.ch>
2023-12-25 16:00:07 +0100

    changes to cards

--- wwwroot/Scripts/main.js
+++ wwwroot/Scripts/main.js
@@ -2,6 +2,18 @@
     document.getElementById("popup-cash").classList.toggle("visible");
 }
 
+function editCard() {
+    document.getElementById("popup-edit-card").classList.toggle("visible");
+}
+
 function newCard() {
     document.getElementById("popup-new-card").classList.toggle("visible");
+}
+
+function removeCard() {
+    document.getElementById("card").remove();
+}
+
+function removeCash() {
+    document.getElementById("cash").remove();
 }
