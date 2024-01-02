231cad77b6ada8ab5136a210a5666f5f8ecd33f7
Damian Moser <damian.moser@sensemail.ch>
2024-01-02 13:02:40 +0100

    change self made pop ups to mudblazor dialogs

--- wwwroot/Scripts/main.js
+++ wwwroot/Scripts/main.js
@@ -1,19 +1,3 @@
-﻿function editCash() {
-    document.getElementById("popup-cash").classList.toggle("visible");
-}
-
-function editCard() {
-    document.getElementById("popup-edit-card").classList.toggle("visible");
-}
-
-function newCard() {
-    document.getElementById("popup-new-card").classList.toggle("visible");
-}
-
-function removeCard() {
-    document.getElementById("card").remove();
-}
-
-function removeCash() {
-    document.getElementById("cash").remove();
+﻿window.removeElement = function(element) {
+    element.remove();
 }
