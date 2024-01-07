978d2d0a2affd4af0f68df30e46a41382f28007c
Damian Moser <dmo140748@stud.gibb.ch>
2024-01-07 15:54:37 +0100

    info: information page

--- wwwroot/Scripts/main.js
+++ wwwroot/Scripts/main.js
@@ -1,3 +1,7 @@
-﻿window.removeElement = function(element) {
+﻿window.removeElement = function (element) {
     element.remove();
 }
+
+document.querySelectorAll(".bilanz").forEach((el) => {
+    el.addEventListener("click", () => window.location.href = "/history#bilanz")
+});
